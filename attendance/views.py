890e0bdde7a37f90c744981e7011d8ea7f800eaf
kangkang <3360976193@qq.com>
2023-09-13 09:10:52 +0800

    变更

--- attendance/views.py
+++ attendance/views.py
@@ -6,9 +6,10 @@ from django.http import JsonResponse
 
 from attendance.models import Attendance
 
+
 # 分页查询
 def getAttendance(request):
-    if request.method!='GET':
+    if request.method != 'GET':
         return JsonResponse({'code': 500, 'msg': '不支持post请求', 'data': None})
     page = int(request.GET.get("page"))
     limit = int(request.GET.get("limit"))
@@ -21,17 +22,18 @@ def getAttendance(request):
 
 #  根据id 查询
 def getById(request):
-    if request.method!='GET':
+    if request.method != 'GET':
         return JsonResponse({'code': 500, 'msg': '不支持post请求', 'data': None})
     id = int(request.GET.get("id"))
     admin = Attendance.objects.filter(id=id)
-    if len(admin)==0:
+    if len(admin) == 0:
         return JsonResponse({'code': 500, 'msg': '未找到', 'data': None})
     return JsonResponse({'code': 0, 'msg': '请求成功', 'data': json.loads(serializers.serialize("json", admin))})
 
+
 # 删除
 def deleted(request):
-    if request.method!='POST':
+    if request.method != 'POST':
         return JsonResponse({'code': 500, 'msg': '不支持GET请求', 'data': None})
     ids = eval(request.POST.get("ids"))
     if len(ids) == 0:
@@ -39,9 +41,10 @@ def deleted(request):
     Attendance.objects.filter(id__in=ids).delete()
     return JsonResponse({'code': 0, 'msg': '删除成功', 'data': None})
 
+
 #  添加
 def save(request):
-    if request.method!='POST':
+    if request.method != 'POST':
         return JsonResponse({'code': 500, 'msg': '不支持GET请求', 'data': None})
     course_name = request.POST.get("course_name")
     late = request.POST.get("late")
@@ -62,11 +65,11 @@ def save(request):
 
 #  修改
 def update(request):
-    if request.method!='POST':
+    if request.method != 'POST':
         return JsonResponse({'code': 500, 'msg': '不支持GET请求', 'data': None})
     id = request.POST.get("id")
     course_name = request.POST.get("course_name")
     late = request.POST.get("late")
     late_time = int(request.POST.get("late_time"))
-    Attendance.objects.filter(id=id).update(course_name=course_name, late=late ,late_time=late_time)
+    Attendance.objects.filter(id=id).update(course_name=course_name, late=late, late_time=late_time)
     return JsonResponse({'code': 0, 'msg': '修改成功', 'data': None})
